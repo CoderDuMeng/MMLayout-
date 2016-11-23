348d3f236e8575cf1e5aa97229b89e83780e36e0
DemoDu <dom@detu.com>
2016-11-23 21:03:24 +0800

    MMLayout

---
+++ UIView+MMLayout.h
@@ -0,0 +1,58 @@
+//
+//  UIView+MM.h
+//  MMCocoa
+//
+/* 
+ 杜蒙 iOS开发 
+ Demo du 
+ 
+ 
+*/
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (MM)
+- (void)setX:(CGFloat)x;
+- (CGFloat)x;
+- (void)setY:(CGFloat)y;
+- (CGFloat)y;
+- (void)setW:(CGFloat)w;
+- (CGFloat)w;
+- (void)setH:(CGFloat)h;
+- (CGFloat)h;
+- (CGFloat)maxY;
+- (CGFloat)minY;
+- (CGFloat)maxX;
+- (CGFloat)minX;
+- (CGFloat)halfW;
+- (CGFloat)halfH;
+- (CGFloat)halfX;
+- (CGFloat)halfY;
+@end
+@interface MMLayout : NSObject
+-(instancetype)initWithLayoutView:(UIView *)LayoutView;
+@property (assign , nonatomic) CGFloat top;    //上
+@property (assign , nonatomic) CGFloat bottom; //下
+@property (assign , nonatomic) CGFloat left;   //左
+@property (assign , nonatomic) CGFloat right;  //右
+@property (assign , nonatomic) CGFloat width;  //右
+@property (assign , nonatomic) CGFloat height;  //右
+@property (assign , nonatomic) CGSize  size;
+
+//距离父控件中心  //<<调用此方法钱必须先设置自己的宽高
+- (void)center;
+
+@end
+
+@interface UIView (Layout)
+
+//设置自己的Layout
+- (void)make_Layout:(void(^)(MMLayout * layout))layout;
+
+@end
+
+
+
+
---
+++ UIView+MMLayout.m
@@ -0,0 +1,125 @@
+
+
+#import "UIView+MM.h"
+
+@implementation UIView (MM)
+- (void)setX:(CGFloat)x{
+    CGRect frame = self.frame;
+    frame.origin.x = x;
+    self.frame = frame;
+}
+- (CGFloat)x{
+    return self.frame.origin.x;
+}
+- (void)setY:(CGFloat)y{
+    CGRect frame = self.frame;
+    frame.origin.y = y;
+    self.frame = frame;
+}
+- (CGFloat)y{
+    return self.frame.origin.y;
+}
+- (void)setW:(CGFloat)w{
+    CGRect frame = self.frame;
+    frame.size.width = w;
+    self.frame = frame;
+}
+- (CGFloat)w{
+    return self.frame.size.width;
+}
+- (void)setH:(CGFloat)h{
+    CGRect frame = self.frame;
+    frame.size.height = h;
+    self.frame = frame;
+}
+- (CGFloat)h{
+    return self.frame.size.height;
+}
+- (CGFloat)maxY{
+    return CGRectGetMaxY(self.frame);
+}
+- (CGFloat)minY{
+    return CGRectGetMinY(self.frame);
+}
+- (CGFloat)maxX{
+    return CGRectGetMaxX(self.frame);
+}
+- (CGFloat)minX{
+    return CGRectGetMinX(self.frame);
+}
+- (CGFloat)halfW{
+    return self.w / 2;
+}
+- (CGFloat)halfH{
+    return self.h / 2;
+}
+- (CGFloat)halfX{
+    return self.x / 2;
+}
+- (CGFloat)halfY{
+    return self.y / 2;
+}
+@end
+@interface MMLayout()
+@property (weak   , nonatomic) UIView *layoutView;
+@end
+
+@implementation MMLayout
+-(instancetype)initWithLayoutView:(UIView *)LayoutView{
+    if (self=[super init]) {
+        self.layoutView = LayoutView;
+    }
+    return self;
+}
+-(void)setLeft:(CGFloat)left{
+    _left = left;
+    self.layoutView.x = left;
+}
+-(void)setTop:(CGFloat)top{
+    _top = top;
+    self.layoutView.y = top;
+}
+-(void)setRight:(CGFloat)right{
+    _right = right;
+   UIView *superview = self.layoutView.superview;
+    self.layoutView.x = superview.w - self.layoutView.w - right;
+}
+
+-(void)setBottom:(CGFloat)bottom{
+    _bottom = bottom;
+    UIView *superview = self.layoutView.superview;
+    self.layoutView.y =  superview.h - self.layoutView.h - bottom;
+}
+
+-(void)setHeight:(CGFloat)height{
+    _height = height;
+    self.layoutView.h  = height;
+    
+}
+-(void)setWidth:(CGFloat)width{
+    _width = width;
+    self.layoutView.w = width;
+}
+-(void)setSize:(CGSize )size{
+    self.layoutView.w = size.width;
+    self.layoutView.h = size.height;
+}
+-(void)center{
+    UIView *superview = self.layoutView.superview;
+    self.layoutView.x = superview.halfW - self.layoutView.halfW;
+    self.layoutView.y = superview.halfH - self.layoutView.halfH;
+}
+@end
+
+@implementation UIView (Layout)
+-(void)make_Layout:(void (^)(MMLayout *))layout{
+    if (layout) {
+        MMLayout *mm_Layout = [[MMLayout alloc] initWithLayoutView:self];
+        layout(mm_Layout);
+    }
+}
+
+@end
+
+
+
