d8e96150b5af6d12400c67062f3cef366dc5335c
杜蒙 <dom@detu.com>
2016-12-29 11:13:28 +0800

    增加相同设置

--- UIView+MMLayout.h
+++ UIView+MMLayout.h
@@ -12,33 +12,34 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (MM)
-- (void)setX:(CGFloat)x; ///<< set frame.x
-- (CGFloat)x;            ///<< get frame.x
-- (void)setY:(CGFloat)y; ///<< set frame.y
-- (CGFloat)y;            ///<< get frame.y
-- (void)setW:(CGFloat)w; ///<< set frame.bounds.size.width
-- (CGFloat)w;            ///<< get frame.bounds.size.width
-- (void)setH:(CGFloat)h; ///<< set frame.bounds.size.height
-- (CGFloat)h;            ///<< get frame.bounds.size.height
-- (CGFloat)centerX;      ///<< get self.center.x
-- (CGFloat)centerY;      ///<< get self.center.y
-- (CGFloat)maxY;         ///<< get CGRectGetMaxY
-- (CGFloat)minY;         ///<< get CGRectGetMinY
-- (CGFloat)maxX;         ///<< get CGRectGetMaxX
-- (CGFloat)minX;         ///<< get CGRectGetMinX
-- (CGFloat)halfW;        ///<< get self.width / 2
-- (CGFloat)halfH;        ///<< get self.height / 2
-- (CGFloat)halfX;        ///<< get self.x / 2
-- (CGFloat)halfY;        ///<< get self.y / 2
-- (CGFloat)halfCenterX;  ///<< get self.centerX / 2
-- (CGFloat)halfCenterY;  ///<< get self.centerY / 2
+- (void)setMm_x:(CGFloat)mm_x; ///<< set frame.x
+- (CGFloat)mm_x;            ///<< get frame.x
+- (void)setMm_y:(CGFloat)mm_y; ///<< set frame.y
+- (CGFloat)mm_y;            ///<< get frame.y
+- (void)setMm_w:(CGFloat)mm_w; ///<< set frame.bounds.size.width
+- (CGFloat)mm_w;            ///<< get frame.bounds.size.width
+- (void)setMm_h:(CGFloat)mm_h; ///<< set frame.bounds.size.height
+- (CGFloat)mm_h;            ///<< get frame.bounds.size.height
+- (CGFloat)mm_centerX;      ///<< get self.center.x
+- (CGFloat)mm_centerY;      ///<< get self.center.y
+- (CGFloat)mm_maxY;         ///<< get CGRectGetMaxY
+- (CGFloat)mm_minY;         ///<< get CGRectGetMinY
+- (CGFloat)mm_maxX;         ///<< get CGRectGetMaxX
+- (CGFloat)mm_minX;         ///<< get CGRectGetMinX
+- (CGFloat)mm_halfW;        ///<< get self.width / 2
+- (CGFloat)mm_halfH;        ///<< get self.height / 2
+- (CGFloat)mm_halfX;        ///<< get self.x / 2
+- (CGFloat)mm_halfY;        ///<< get self.y / 2
+- (CGFloat)mm_halfCenterX;  ///<< get self.centerX / 2
+- (CGFloat)mm_halfCenterY;  ///<< get self.centerY / 2
+- (void)setMm_size:(CGSize)mm_size; 
 - (CGSize)mm_size;       ///<< get self.bounds.size
 
 
 @end
 @interface MMLayout : NSObject
 /* 
-  @LayoutView   传入进去seting View
+  @LayoutView   传入进去  View
 */
 -(instancetype)initWithLayoutView:(UIView *)LayoutView;
 @property (assign , nonatomic) CGFloat top;    ///<<  frame y
@@ -52,6 +53,8 @@
 @end
 @interface UIView (Layout)
 
+@property (strong , nonatomic ,  readonly ) MMLayout *mm_selfLayout;
+
 /*
  [self make_Layout:^(MMLayout *layout) {
  layout.width  = 100;
@@ -62,26 +65,36 @@
  }];
 */
 - (void)make_Layout:(void(^)(MMLayout * layout))layout;
-
-/* 
+/*
   当frmae 将要属性发生变化的时候会调用此方法
 */
 - (void)make_LayoutUpdateChange:(void(^)(CGRect frame))block;
-
 /*
    示例链接编程
  
    self.width(100).height(100).left(10).top(10)
  
 */
-- (UIView *(^)(CGFloat top))m_top;            ///< set frame y
-- (UIView *(^)(CGFloat bottom))m_bottom;      ///< set frame y
-- (UIView *(^)(CGFloat left))m_left;          ///< set frame x
-- (UIView *(^)(CGFloat right))m_right;        ///< set frame x
-- (UIView *(^)(CGFloat width))m_width;        ///< set frame width
-- (UIView *(^)(CGFloat height))m_height;      ///< set frame height
-- (UIView *(^)(CGSize size))m_size;           ///< set frame size
-- (UIView *(^)())m_center;                 ///< set frame center  前提是有w h 调用次方法居中父类
+- (UIView * (^)(CGFloat top))m_top;            ///< set frame y
+- (UIView * (^)(CGFloat bottom))m_bottom;      ///< set frame y
+- (UIView * (^)(CGFloat left))m_left;          ///< set frame x
+- (UIView * (^)(CGFloat right))m_right;        ///< set frame x
+- (UIView * (^)(CGFloat width))m_width;        ///< set frame width
+- (UIView * (^)(CGFloat height))m_height;      ///< set frame height
+- (UIView * (^)(CGSize size))m_size;           ///< set frame size
+- (UIView * (^)())m_center;                    ///< set frame center  前提是有w h 调用次方法居中父类
+
+
+
+- (UIView * (^)(UIView *obj))m_equalToFrame;   /// frame
+- (UIView * (^)(UIView *obj))m_equalToTop;     /// top
+- (UIView * (^)(UIView *obj))m_equalToBottom;  /// Bottom
+- (UIView * (^)(UIView *obj))m_equalToLeft;    /// left
+- (UIView * (^)(UIView *obj))m_equalToRight;   /// right
+- (UIView * (^)(UIView *obj))m_equalToWidth;   /// width
+- (UIView * (^)(UIView *obj))m_equalToHeight;  /// height
+- (UIView * (^)(UIView *obj))m_equalToSize;    /// size
+
 
 @end
 
--- UIView+MMLayout.m
+++ UIView+MMLayout.m
@@ -3,79 +3,88 @@
 #import "UIView+MMLayout.h"
 #import <objc/runtime.h>
 @implementation UIView (MM)
-- (void)setX:(CGFloat)x{
+#pragma mark - frame
+- (void)setMm_x:(CGFloat)mm_x{
     CGRect frame = self.frame;
-    frame.origin.x = x;
+    frame.origin.x = mm_x;
     self.frame = frame;
 }
-- (CGFloat)x{
+- (CGFloat)mm_x{
     return self.frame.origin.x;
 }
-- (void)setY:(CGFloat)y{
+- (void)setMm_y:(CGFloat)mm_y{
     CGRect frame = self.frame;
-    frame.origin.y = y;
+    frame.origin.y = mm_y;
     self.frame = frame;
 }
-- (CGFloat)y{
+- (CGFloat)mm_y{
     return self.frame.origin.y;
 }
-- (void)setW:(CGFloat)w{
+- (void)setMm_w:(CGFloat)mm_w{
     CGRect frame = self.frame;
-    frame.size.width = w;
+    frame.size.width = mm_w;
     self.frame = frame;
 }
-- (CGFloat)w{
+- (CGFloat)mm_w{
     return self.frame.size.width;
 }
-- (void)setH:(CGFloat)h{
+- (void)setMm_h:(CGFloat)mm_h{
     CGRect frame = self.frame;
-    frame.size.height = h;
+    frame.size.height = mm_h;
     self.frame = frame;
+   
 }
-- (CGFloat)h{
+- (CGFloat)mm_h{
     return self.frame.size.height;
 }
--(CGFloat)centerX{
+-(CGFloat)mm_centerX{
     return self.center.x;
 }
--(CGFloat)centerY{
+-(CGFloat)mm_centerY{
     return self.center.y;
 }
-- (CGFloat)maxY{
+- (CGFloat)mm_maxY{
     return CGRectGetMaxY(self.frame);
 }
-- (CGFloat)minY{
+- (CGFloat)mm_minY{
     return CGRectGetMinY(self.frame);
 }
-- (CGFloat)maxX{
+- (CGFloat)mm_maxX{
     return CGRectGetMaxX(self.frame);
 }
-- (CGFloat)minX{
+- (CGFloat)mm_minX{
     return CGRectGetMinX(self.frame);
 }
-- (CGFloat)halfW{
-    return self.w / 2;
+- (CGFloat)mm_halfW{
+    return self.mm_w / 2;
 }
-- (CGFloat)halfH{
-    return self.h / 2;
+- (CGFloat)mm_halfH{
+    return self.mm_h / 2;
 }
-- (CGFloat)halfX{
-    return self.x / 2;
+- (CGFloat)mm_halfX{
+    return self.mm_x / 2;
 }
-- (CGFloat)halfY{
-    return self.y / 2;
+- (CGFloat)mm_halfY{
+    return self.mm_y / 2;
 }
--(CGFloat)halfCenterX{
-    return self.centerX / 2;
+-(CGFloat)mm_halfCenterX{
+    return self.mm_centerX / 2;
 }
--(CGFloat)halfCenterY{
-   return self.centerY / 2;
+-(CGFloat)mm_halfCenterY{
+   return self.mm_centerY / 2;
+}
+-(void)setMm_size:(CGSize)mm_size{
+    CGRect frame = self.frame;
+    frame.size = mm_size;
+    self.frame = frame;
 }
 -(CGSize)mm_size{
     return self.bounds.size;
 }
 
 @end
+#pragma mark - MMLayout
+
 @interface MMLayout()
 @property (copy   , nonatomic) void(^updateChangeBlock)(CGRect frame);
 @property (weak   , nonatomic) UIView *layoutView;
@@ -90,54 +99,61 @@
 }
 -(void)setLeft:(CGFloat)left{
     _left = left;
-    self.layoutView.x = left;
+    self.layoutView.mm_x = left;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
 -(void)setTop:(CGFloat)top{
     _top = top;
-    self.layoutView.y = top;
+    self.layoutView.mm_y = top;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
 -(void)setRight:(CGFloat)right{
     _right = right;
    UIView *superview = self.layoutView.superview;
-    self.layoutView.x = superview.w - self.layoutView.w - right;
+    self.layoutView.mm_x = superview.mm_w - self.layoutView.mm_w - right;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
 
 -(void)setBottom:(CGFloat)bottom{
     _bottom = bottom;
     UIView *superview = self.layoutView.superview;
-    self.layoutView.y =  superview.h - self.layoutView.h - bottom;
+    self.layoutView.mm_y =  superview.mm_h - self.layoutView.mm_h - bottom;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
 
 -(void)setHeight:(CGFloat)height{
     _height = height;
-    self.layoutView.h  = height;
+    self.layoutView.mm_h  = height;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
 -(void)setWidth:(CGFloat)width{
     _width = width;
-    self.layoutView.w = width;
+    self.layoutView.mm_w = width;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 }
+
 -(void)setSize:(CGSize )size{
-    self.layoutView.w = size.width;
-    self.layoutView.h = size.height;
+    self.layoutView.mm_size = size;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
 
 }
+-(CGSize)size{
+    return self.layoutView.mm_size;
+}
+
 -(void)center{
     UIView *superview = self.layoutView.superview;
-    self.layoutView.x = superview.halfW - self.layoutView.halfW;
-    self.layoutView.y = superview.halfH - self.layoutView.halfH;
+    self.layoutView.mm_x = superview.mm_halfW - self.layoutView.mm_halfW;
+    self.layoutView.mm_y = superview.mm_halfH - self.layoutView.mm_halfH;
     !self.updateChangeBlock ? : self.updateChangeBlock(self.layoutView.frame);
     
 }
 @end
 const void *_layoutKey;
 @implementation UIView (Layout)
+
+#pragma mark - set_ frame
+
 -(void)make_LayoutUpdateChange:(void (^)(CGRect))block{
     [self mm_selfLayout].updateChangeBlock = block;
 }
@@ -157,6 +173,7 @@ const void *_layoutKey;
     }
     return layout;
 }
+
 
 -(UIView *(^)(CGFloat))m_top{
     __weak typeof(self)_self = self;
@@ -226,6 +243,80 @@ const void *_layoutKey;
         return self;
     };
 }
+
+#pragma mark -  m_equalTo
+-(UIView *(^)(UIView *))m_equalToFrame{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.frame = obj.frame;
+        return self;
+    };
+}
+-(UIView *(^)(UIView *))m_equalToTop{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.top = obj.mm_selfLayout.top;
+        return self;
+    };
+    
+}
+-(UIView *(^)(UIView *))m_equalToBottom{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.bottom = obj.mm_selfLayout.bottom;
+        return self;
+    };
+}
+-(UIView *(^)(UIView *))m_equalToLeft{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.left = obj.mm_selfLayout.left;
+        return self;
+    };
+    
+}
+-(UIView *(^)(UIView *))m_equalToRight{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.right = obj.mm_selfLayout.right;
+        return self;
+    };
+
+}
+-(UIView *(^)(UIView *))m_equalToWidth{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.width = obj.mm_selfLayout.width;
+        return self;
+    };
+}
+
+-(UIView *(^)(UIView *))m_equalToHeight{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        self.mm_selfLayout.height = obj.mm_selfLayout.height;
+        return self;
+    };
+}
+-(UIView *(^)(UIView *))m_equalToSize{
+    __weak typeof(self)_self  = self;
+    return ^(UIView *obj){
+        __strong typeof(_self)self = _self;
+        [self mm_selfLayout].size = obj.mm_selfLayout.size;
+        return self;
+    };
+
+}
+
+
+
 @end
 
 
